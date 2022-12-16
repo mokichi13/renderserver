7166c1a4d6beaaf6f2390fb383e178b658fa40ab
mokichi13 <woodgood98@gmail.com>
2022-12-16 22:24:46 +0900

    add time endpoint

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
+	})
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+
+		now := strconv.Itoa(int(time.Now().Unix()))
+		w.Write([]byte(now))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
